1add38b812d3bf489eced6d01509eec43a7728c6
muaz-kedir <166374226+muaz-kedir@users.noreply.github.com>
2024-07-24 12:24:40 +0300

    test

---
+++ evangadi/nav-bar.js
@@ -0,0 +1,21 @@
+let country = 'ethiopi'
+let continet = 'africa'
+let population = 150000000;
+console.log("my name is:MUAZ KEDIR and my country is:",country,"and ny continent is:",continet, "and also my population is:",population)
+
+let isIsland = false;
+let language;
+language='amharic'
+
+console.log("my name is:MUAZ KEDIR and my country is:", country, "and ny continent is:", continet, "and also my population is:", population, 'and it is ', isIsland, 'tha it is island', 'and our languge is ', language)
+
+let ourLanguage='afaan oromo'
+console.log("my name is:MUAZ KEDIR and my country is:", country, "and ny continent is:", continet, "and also my population is:", population, 'and it is ', isIsland, 'tha it is island', 'and our languge is ', 'and we speek', ourLanguage, 'language')
+console.log(population / 2)
+console.log(population += 1)
+const finland = 6000000;
+const averageOfpeople = 33000000;
+console.log(finland > population)
+console.log(averageOfpeople > population) 
+var discription = 'portugal is in europ and it is:{finland}population';
+console.log(discription)
